eea8f9ce741fc9c5e9af541db51a14eff69897d8
Misterodd <mamertens99@googlemail.com>
2019-04-16 18:49:09 +0200

    Update 0VidsDownloader
    removed --newline in youtube-dl command

--- Windows/0VideoDownloader.bat
+++ Windows/0VideoDownloader.bat
@@ -1,7 +1,7 @@
 
 @echo off
 chcp 1252 && setlocal EnableDelayedExpansion && 0youtube-dl -U && set dg=1000000000
-REM if "%1"=="" ( goto loaded ) 
+if NOT DEFINED %1 ( goto loaded ) 
 if "%1"=="auto" goto :auto 
 if "%1"=="music" goto :music 
 if "%1"=="help" goto :help 
@@ -31,11 +31,11 @@ if "%txt%"=="" echo OH NEIN, hab falsch programmiert F002! && pause && goto fine
 echo "Möchtest du nur Musik herunterladen?"
 set /P m="[J]a oder [N]ein? "
 if /I "%m%"=="j" goto :music
-echo "Möchtest du die Qualität vollständig selbst wählen?" 
+echo "Willst du die Qualität vollständig selbst wählen?" 
 set /P man="[J]a oder [N]ein? "
 if /I "%man%"=="j" goto manual
 echo "OK, dennoch muss ich paar Dinge klären:" & echo "Welches Dateiformat soll genutzt werden? " & echo "[1] Einfach das beste" & echo "[2] webm für allerhöchste quali (bis zu 4k) oder "&echo "[3] mp4 für maximal HD"
-	set /P f=" 1 oder 2 oder 3? "
+	set /P f=" 1 oder 2 oder 3?"-
 	if /I "%f%"=="1" goto best
 	if /I "%f%"=="2" goto bestMP4
 	if /I "%f%"=="3" goto bestWEBM
@@ -44,12 +44,12 @@ goto ende
 	
 :bestMP4
 echo "Lade jetzt die ganzen Daten in mp4. Das dauert etwas..."
-0youtube-dl -r %dg% %q% --console-title --no-warnings -i -f bestvideo[ext=mp4]+bestaudio[ext=mp4]/bestvideo[ext=mp4]+bestaudio[ext=m4a] --all-subs --embed-subs --youtube-skip-dash-manifest  --merge-output-format mp4 %txt% %link%
+0youtube-dl -r %dg% %q% --console-title --no-warnings -u mamertens99 -p Leon1950 --mark-watched -i -f bestvideo[ext=mp4]+bestaudio[ext=mp4]/bestvideo[ext=mp4]+bestaudio[ext=m4a] --all-subs --embed-subs --youtube-skip-dash-manifest  --merge-output-format mp4 %txt% %link%
 goto ende
 
 :bestWEBM
 echo "Lade jetzt die ganzen Daten in webm. Das dauert etwas..."
-0youtube-dl -r %dg% %q% --console-title --no-warnings -i -f bestvideo[ext=webm]+bestaudio[ext=webm] --all-subs --embed-subs --youtube-skip-dash-manifest  --merge-output-format webm %txt% %link%
+0youtube-dl -r %dg% %q% --console-title --no-warnings -u mamertens99 -p Leon1950 --mark-watched -i -f bestvideo[ext=webm]+bestaudio[ext=webm] --all-subs --embed-subs --youtube-skip-dash-manifest  --merge-output-format webm %txt% %link%
 goto ende
 
 :manual
@@ -73,27 +73,26 @@ if %fm%==webm set ad=bestaudio[ext=webm] && goto j1
 set ad=bestaudio[ext=mp4]/bestvideo[ext=mp4]+bestaudio[ext=m4a]
 :j1
 echo es wurde das Audioformat %ad% gewählt.
-0youtube-dl -r %dg%K %q% --newline --console-title --no-warnings -i -f %ad%+%ad% --all-subs --embed-subs  --merge-output-format %fm% %txt% %link%
+0youtube-dl -r %dg%K %q% --console-title -u mamertens99 -p Leon1950 --mark-watched --no-warnings -i -f %ad%+%ad% --all-subs --embed-subs  --merge-output-format %fm% %txt% %link%
 goto ende
 
 :best
-0youtube-dl -r %dg%K %q% -i --all-subs --console-title --embed-subs -f best %txt% %link%
+0youtube-dl -r %dg%K %q% -i -u mamertens99 -p Leon1950 --mark-watched --all-subs --console-title --embed-subs -f best %txt% %link%
 
 goto ende
 
 :auto
-0youtube-dl -r 800K -q --console-title --no-warnings -i -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo[ext=mp4]+bestaudio[ext=mp4] --all-subs --embed-subs --youtube-skip-dash-manifest  --merge-output-format mp4 -a 0VidTHIS.txt
+0youtube-dl -r 800K -q --console-title -u mamertens99 -p Leon1950 --mark-watched --no-warnings -i -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo[ext=mp4]+bestaudio[ext=mp4] --all-subs --embed-subs --youtube-skip-dash-manifest  --merge-output-format mp4 -a 0VidTHIS.txt
 goto finende
 
 :music
 echo "Ich suche in 0vids.txt in diesem Ordner noch Videos" && pause
-0youtube-dl -r %dg%K -q -x --embed-thumbnail --add-metadata --console-title --audio-quality 0 --geo-bypass -i -o %(autonumber)s-%(title)s_by_%(uploader)s.%(ext)s  --audio-format mp3 -a 0vids.txt 
+0youtube-dl -r %dg%K -q -x --embed-thumbnail -u mamertens99 -p Leon1950 --mark-watched --add-metadata --console-title --audio-quality 0 --geo-bypass -i -o %(autonumber)s-%(title)s_by_%(uploader)s.%(ext)s  --audio-format mp3 -a 0vids.txt 
 goto ende
 
 :help
 echo "Hi ich bin die Hilfe. Bitte achte darauf, dass entweder die 0youtube-dl.exe Datei in C:\Windows"
 echo "oder im Ordner hast wo du dieses Script ausführst."
-echo "Videos von der ARD Seite können zur zeit nicht heruntergeladen werden, aber vom WDR."
 echo "Du kannst dieses Script mit folgenede Kommandos nutzen: "
 echo auto :
 echo "Dies führt dazu, dass die Links in der Datei '0vids.txt' automatisch in höchstens HD heruntergeldaen werden. Die 0vids.txt Datei muss dort sein wo dieses Script ausgeführt wird.
@@ -106,7 +105,7 @@ set /P doo="Normal fortfahren? [j/n] "
 if /I "%doo%"=="j" goto loaded
 goto :finende
 :ende
-	 endlocal & echo. & echo "So das wars, im Ordner wo du das hier aktiviert hast liegen jetzt die Video(s). Öffne die Videos am besten mit VLC Du kanst das  hier schließen." & call 0systemTrayNotification.exe -tooltip warning -time 300000 -title "Fertig" -text "So das wars, im Ordner wo du das hier aktiviert hast liegen jetzt die Video(s). Öffne die Videos am besten mit VLC" -icon question &	 pause
+	endlocal & echo. & echo "So das wars, im Ordner wo du das hier aktiviert hast liegen jetzt die Video(s). Öffne die Videos am besten mit VLC Du kanst das  hier schließen." & call systemTrayNotification.exe -tooltip warning -time 300000 -title "Fertig" -text "So das wars, im Ordner wo du das hier aktiviert hast liegen jetzt die Video(s). Öffne die Videos am besten mit VLC" -icon question & pause
 		:finende
 	
 	
